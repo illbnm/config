4063335c94e8c8b602c23e486e80c6475556ff02
MIMO <358901007@qq.com>
2023-04-27 20:40:11 +0800

    Update AutoPull.sh

--- AutoPull.sh
+++ AutoPull.sh
@@ -4,10 +4,10 @@
 IMAGE_NAME=$1
 
 # Pull the image
-docker pull dockerproxy.com/library/$IMAGE_NAME:latest
+docker pull dockerproxy.com/library/$IMAGE_NAME
 
 # Tag the image with the new name
-docker tag dockerproxy.com/library/$IMAGE_NAME:latest $IMAGE_NAME:latest
+docker tag dockerproxy.com/library/$IMAGE_NAME $IMAGE_NAME
 
 # Remove the original image
-docker rmi dockerproxy.com/library/$IMAGE_NAME:latest
+docker rmi dockerproxy.com/library/$IMAGE_NAME
